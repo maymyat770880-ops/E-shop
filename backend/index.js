3ac3ca126d59dacc8de6eacc52375c61aafe059f
May Myat Noe <maymyat770880@gmail.com>
2026-03-25 23:16:18 +0630

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const bcrypt = require('bcryptjs');
 const cors = require('cors');
-require('dotenv').config();
+// require('dotenv').config();
 
 const db = mysql.createConnection({
   host: process.env.MYSQLHOST,
